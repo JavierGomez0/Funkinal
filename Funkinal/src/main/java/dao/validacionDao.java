c867cc400beb9257a4f4955a2e228ba0f833a1b5
azacarias-dev <azacarias-2024007@kinal.edu.gt>
2025-07-23 11:25:24 -0600

    Se agrego la carpeta Dao

--- Funkinal/src/main/java/dao/validacionDao.java
+++ Funkinal/src/main/java/dao/validacionDao.java
@@ -4,10 +4,33 @@
  */
 package dao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import model.Usuarios;
+
 /**
  *
  * @author informatica
  */
 public class validacionDao {
     
+    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("funkinalPU");
+    
+    public void agregar(Usuarios usuario){
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction transaccion = em.getTransaction();
+        
+        try {
+            transaccion.begin();
+            em.persist(usuario);
+            transaccion.commit();
+        } catch (Exception e) {
+            if (transaccion.isActive())
+                transaccion.rollback();
+        }finally {
+            em.close();
+        }
+    }
 }
